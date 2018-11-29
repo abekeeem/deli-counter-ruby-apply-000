8e78f986f03aee81113cc9874bce2662212aa62d
bionic-request-3438 <thunderhorsesemail@gmail.com>
2018-11-29 01:00:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(katz_deli) 
+  if katz_deli.length == 0 
+    puts "The line is currently empty." 
+  end 
+
+  katz_deli.each_with_index do |ele, idx| 
+    puts "The line is currently: #{idx}. #{ele}."
+  end 
+end 
